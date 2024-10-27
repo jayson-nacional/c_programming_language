d8782ec578c0a3462e2ef28a9a35f96f649e9d22
Jayson Nacional <jayson.o.nacional@gmail.com>
2024-10-27 23:11:41 +0800

    Grouping symbol matching exercise draft

---
+++ chapter_01/exercise_1-24.c
@@ -0,0 +1,39 @@
+/* Write a program to check a C program for rudimentary syntax errors like
+ * unmatched parentheses, brackets and braces. Don't forget about quotes, both
+ * single and double, escape sequences, and comments. */
+
+#include<stdio.h>
+
+int main() {
+	/* thought process 
+	* check for matching parentheses, brackets, braces
+	* check for quotes, single and double
+	* handle comments */
+	char grouping_symbols[1000];
+	int c;
+
+	int i = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == '(' || c == '[' || c == '{') {
+			grouping_symbols[i] = c;
+			i++;
+		} else if (c == ')') {
+			if (grouping_symbols[i - 1] == '(')
+				i--;
+			else
+			 printf("Unmatched ( symbol");
+		} else if (c == ']') {
+			if (grouping_symbols[i - 1] == '[')
+				i--;
+			else
+			 printf("Unmatched [ symbol");
+		} else if (c == '}') {
+			if (grouping_symbols[i - 1] == '{')
+				i--;
+			else
+			 printf("Unmatched { symbol");
+		}
+	}
+
+	return 0;
+}
