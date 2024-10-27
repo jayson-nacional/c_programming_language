1c06e17ee8bca276b8174b151f497d3d9001e607
Jayson Nacional <jayson.o.nacional@gmail.com>
2024-10-27 23:12:41 +0800

    Grouping symbol matching exercise draft

--- chapter_01/exercise_1-24.c
+++ chapter_01/exercise_1-24.c
@@ -2,38 +2,44 @@
  * unmatched parentheses, brackets and braces. Don't forget about quotes, both
  * single and double, escape sequences, and comments. */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-	/* thought process 
-	* check for matching parentheses, brackets, braces
-	* check for quotes, single and double
-	* handle comments */
-	char grouping_symbols[1000];
-	int c;
+  /* thought process
+   * check for matching parentheses, brackets, braces
+   * check for quotes, single and double
+   * handle comments */
+  char grouping_symbols[1000];
+  int c;
 
-	int i = 0;
-	while ((c = getchar()) != EOF) {
-		if (c == '(' || c == '[' || c == '{') {
-			grouping_symbols[i] = c;
-			i++;
-		} else if (c == ')') {
-			if (grouping_symbols[i - 1] == '(')
-				i--;
-			else
-			 printf("Unmatched ( symbol");
-		} else if (c == ']') {
-			if (grouping_symbols[i - 1] == '[')
-				i--;
-			else
-			 printf("Unmatched [ symbol");
-		} else if (c == '}') {
-			if (grouping_symbols[i - 1] == '{')
-				i--;
-			else
-			 printf("Unmatched { symbol");
-		}
-	}
+  int i = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == '(' || c == '[' || c == '{') {
+      grouping_symbols[i] = c;
+      i++;
+    } else if (c == ')') {
+      if (i <= 0)
+        printf("Unmatched ) symbol");
+      else if (grouping_symbols[i - 1] == '(')
+        i--;
+      else
+        printf("Unmatched ( symbol");
+    } else if (c == ']') {
+      if (i <= 0)
+        printf("Unmatched ] symbol");
+      else if (grouping_symbols[i - 1] == '[')
+        i--;
+      else
+        printf("Unmatched [ symbol");
+    } else if (c == '}') {
+      if (i <= 0)
+        printf("Unmatched ] symbol");
+      else if (grouping_symbols[i - 1] == '{')
+        i--;
+      else
+        printf("Unmatched { symbol");
+    }
+  }
 
-	return 0;
+  return 0;
 }
