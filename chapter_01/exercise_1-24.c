3bc7988746c3737bfd58b6820b1cab17d041bcb4
Jayson Nacional <jayson.o.nacional@gmail.com>
2024-10-28 22:55:12 +0800

    Syntax checking exercise

--- chapter_01/exercise_1-24.c
+++ chapter_01/exercise_1-24.c
@@ -4,41 +4,59 @@
 
 #include <stdio.h>
 
+#define IN_CODE 1
+#define IN_STRING 2
+#define IN_CHAR 3
+
 int main() {
   /* thought process
-   * check for matching parentheses, brackets, braces
-   * check for quotes, single and double
+   * escape sequences and single quotes
    * handle comments */
   char grouping_symbols[1000];
   int c;
 
+  int state = IN_CODE;
   int i = 0;
   while ((c = getchar()) != EOF) {
-    if (c == '(' || c == '[' || c == '{') {
+    if (c == '"' && state == IN_CODE)
+      state = IN_STRING;
+    else if (c == '"' && state == IN_STRING)
+      state = IN_CODE;
+    else if (c == '\'' && state == IN_CODE)
+      state = IN_CHAR;
+    else if (c == '\'' && state == IN_CHAR)
+      state = IN_CODE;
+    else if (state == IN_CODE && (c == '(' || c == '[' || c == '{')) {
       grouping_symbols[i] = c;
       i++;
-    } else if (c == ')') {
+    } else if (state == IN_CODE && c == ')') {
       if (i <= 0)
-        printf("Unmatched ) symbol");
+        printf("Unmatched ) symbol\n");
       else if (grouping_symbols[i - 1] == '(')
         i--;
       else
-        printf("Unmatched ( symbol");
-    } else if (c == ']') {
+        printf("Unmatched ( symbol\n");
+    } else if (state == IN_CODE && c == ']') {
       if (i <= 0)
-        printf("Unmatched ] symbol");
+        printf("Unmatched ] symbol\n");
       else if (grouping_symbols[i - 1] == '[')
         i--;
       else
-        printf("Unmatched [ symbol");
-    } else if (c == '}') {
+        printf("Unmatched [ symbol\n");
+    } else if (state == IN_CODE && c == '}') {
       if (i <= 0)
-        printf("Unmatched ] symbol");
+        printf("Unmatched } symbol\n");
       else if (grouping_symbols[i - 1] == '{')
         i--;
       else
-        printf("Unmatched { symbol");
+        printf("Unmatched { symbol\n");
     }
+  }
+
+  printf("Value of i: %d", i);
+  if (i > 0) {
+    printf("Last value of unmatched symbol: %d\n", grouping_symbols[i]);
+    putchar('\n');
   }
 
   return 0;
---
+++ introduction/experiment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  char c = '{';
+  putchar(c);
+  putchar('\n');
+
+  printf("Numerical value: %d\n", c);
+  return 0;
+}
