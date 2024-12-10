4bb42102fd1fd49d3800dbd6310740c0b5faccca
Jayson Nacional <jayson.o.nacional@gmail.com>
2024-12-10 23:15:38 +0800

    Squeeze string exercise

---
+++ chapter_02/exercise_2-4.c
@@ -0,0 +1,50 @@
+/* Write an alternative version of squeeze(s1, s2) that deletes each character
+ * in s1 that matches any character in the string s2 */
+#include <stdio.h>
+
+void squeeze_v1(char s[], char c);
+void squeeze(char s1[], char s2[]);
+int get_char_index(char s[], char c);
+
+int main() {
+  printf("Testing program...\n");
+  char line[100];
+  line[0] = 'a';
+  line[1] = 'b';
+  line[2] = 'c';
+  line[3] = '\0';
+
+  int char_index = get_char_index(line, 'd');
+  printf("Char index: %d\n", char_index);
+  return 0;
+}
+
+void squeeze(char s1[], char s2[]) {
+  int j = 0;
+
+  for (int i = 0; s1[i] != '\0'; i++) {
+    if (get_char_index(s2, s1[i]) != -1)
+      s1[j++] = s1[i];
+  }
+
+  s1[j] = '\0';
+}
+
+void squeeze_v1(char s[], char c) {
+  int j = 0;
+
+  for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i] != c)
+      s[j++] = c;
+  }
+
+  s[j] = '\0';
+}
+
+int get_char_index(char s[], char c) {
+  for (int i = 0; s[i] != '\0'; i++)
+    if (s[i] == c)
+      return i;
+
+  return -1;
+}
