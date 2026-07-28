25c51d3674b6afdab8ed7939d8d90d72a04263de
Jayson Nacional <jayson.o.nacional@gmail.com>
2026-07-28 14:20:03 +0000

    exercises review

--- chapter_1/sandbox.c
+++ chapter_1/sandbox.c
@@ -3,12 +3,15 @@
 int get_line(char s[]);
 void merge_spaces();
 void word_count();
+void one_word_per_line();
+void horizontal_word_histogram();
+void vertical_word_histogram();
 
 #define OUT 0
 #define IN 1
 
 int main() {
-  word_count();
+  vertical_word_histogram();
   return 0;
 }
 
@@ -52,4 +55,76 @@ void word_count() {
   }
 
   printf("The number of words in input is %d\n", wc);
+}
+
+void one_word_per_line() {
+  int c, p; // where c = current char, p = previous char
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n') {
+      if (p != ' ' && p != '\t' && p != '\n')
+        putchar('\n');
+    } else {
+      putchar(c);
+    }
+
+    p = c;
+  }
+}
+
+void horizontal_word_histogram() {
+  int c, p; // where c = current char, p = previous char
+  int input_length = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n') {
+      if (p != ' ' && p != '\t' && p != '\n')
+        putchar('\n');
+    } else {
+      putchar('*');
+    }
+
+    p = c;
+    ++input_length;
+  }
+
+  for (int i = 0; i < input_length; i++)
+    putchar('*');
+
+  putchar('\n');
+}
+
+void vertical_word_histogram() {
+  int c, p; // where c = current char, p = previous char
+  int input_length = 0;
+  char word_lengths[1000];
+  int length_index = 0;
+
+  for (; length_index < 1000; length_index++)
+    word_lengths[length_index] = 0;
+
+  length_index = 0;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n') {
+      if (p != ' ' && p != '\t' && p != '\n')
+        ++length_index;
+    } else {
+      ++word_lengths[length_index];
+    }
+
+    p = c;
+    ++input_length;
+  }
+
+  while (input_length > 0) {
+    putchar('*');
+
+    for (length_index = 0; word_lengths[length_index] != 0; length_index++) {
+      if (input_length > word_lengths[length_index])
+        putchar(' ');
+      else
+        putchar('*');
+    }
+
+    putchar('\n');
+    --input_length;
+  }
 }
